0f6924ac6b9919a355424db203422897c811c985
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2020-03-09 20:26:28 +0900

    Update score_ranking.js

--- score_ranking.js
+++ score_ranking.js
@@ -1,19 +1,28 @@
 javascript:(function(){
     const href=location.href;
-    var match=href.match("https://mypage.groovecoaster.jp/sp/#/se_rg/");
+    const host="https://mypage.groovecoaster.jp";
+    var match=href.match(host+"/sp/#/se_rg/");
     if(match==null){
         alert("スコアランキング画面で実行してください");
     }
     else{
+        var xmlHttp=new XMLHttpRequest();
         const key=href.replace(/[^0-9]/g,'');
         const music_id=Math.floor(key/10);
-        const difficulty=key-music_id*10;
+        const difficulty=["(simple)","(normal)","(hard)","(extra)"];
+        const d_idx=key-music_id*10;
         const ide_score=1000000;
         var page=0;
         var flag=0;
+        
+        xmlHttp.open("GET",host+"/sp/json/music_detail.php?music_id="+music_id,false);
+        xmlHttp.send(null);
+        var data=JSON.parse(xmlHttp.responseText);
+        const music_title=data.music_detail.music_title;
+        
+        //max:30page
         while(!flag&&page<30){
-            var xmlHttp=new XMLHttpRequest();
-            xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id="+music_id+"&difficulty="+difficulty+"&page="+page,false);
+            xmlHttp.open("GET",host+"/sp/json/score_ranking_bymusic_bydifficulty.php?music_id="+music_id+"&difficulty="+d_idx+"&page="+page,false);
             xmlHttp.send(null);
             var data=JSON.parse(xmlHttp.responseText);
             console.log(data);
@@ -24,25 +33,13 @@ javascript:(function(){
             }
             page++;
         }
+        
         var i;
         for(i=0;i<ary_len-1;i++){
             if(data.score_rank[i].event_point<ide_score) break;
         }
         var ide_num=page*100+i;
-        alert("理論値人数:"+ide_num+"人");
-        /*
-        console.log(data.music_detail);
-        const path=[data.music_detail.simple_result_data,data.music_detail.normal_result_data,data.music_detail.hard_result_data,data.music_detail.extra_result_data];
-        const diff=["simple:","normal:","hard:","extra:"];
-        var alert_disp="%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE"+data.music_detail.music_title+"%E3%81%AEperfect%E5%9B%9E%E6%95%B0";
-        var perfect_num;
-        var diff_lng=diff.length;
-        if(!data.music_detail.ex_flag) diff_lng--;
-        for(var i=0;i<diff_lng;i++){
-            (path[i]==null) ? perfect_num="%E6%9C%AA%E3%83%97%E3%83%AC%E3%82%A4" : perfect_num=path[i].perfect+"%E5%9B%9E";
-            alert_disp+="\n"+diff[i]+perfect_num;
-        }
-        alert(alert_disp);
-        */
+        var sum=data.count;
+        alert(music_title+difficulty[d_idx]+"の理論値人数\n"+sum+"人中"+ide_num+"人");
     }
 })();
